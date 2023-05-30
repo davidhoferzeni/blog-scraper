2963cad172844841639e0dd93fce03c8250923e4
David Hofer-Zeni <david_hofer-zeni@hotmail.com>
2023-05-30 14:29:17 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,127 @@
+import puppeteer from 'puppeteer';
+import Epub from 'epub-gen';
+import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
+import blogconfig from './blogconfig.json' assert { type: 'json' };
+
+/**
+ * My cool function.
+ *
+ * @param {Object} b - Blog object.
+ * @param {string} b.url
+ * @param {string} b.titleSelector
+ * @param {string} b.articleSelector
+ * @param {string} b.contentSelector
+ */
+async function scrapeArticle({
+    url,
+    articleSelector,
+    titleSelector,
+    contentSelector,
+}) {
+    let browser;
+    try {
+        browser = await puppeteer.launch({ headless: 'new' });
+        const page = await browser.newPage();
+        page.setDefaultNavigationTimeout(2 * 60 * 1000);
+        await page.goto(url);
+        await page.waitForSelector(titleSelector);
+        const title =
+            (await page.$eval(
+                titleSelector,
+                (element) => element.textContent
+            )) || 'No title';
+        await page.waitForSelector(articleSelector);
+        const article = await page.$(articleSelector);
+        const paragraphs = await article?.$$eval(contentSelector, (elements) =>
+            elements.map((el) => el.outerHTML)
+        );
+        const data = paragraphs?.join('\n') || 'No conent';
+        return {
+            title,
+            data,
+        };
+    } catch (e) {
+        console.error('scrape failed', e);
+    } finally {
+        await browser?.close();
+    }
+}
+
+/**
+ *
+ * @param {string} title
+ * @param {import("epub-gen").Chapter[]} chapters
+ * @param {string} [output]
+ * @returns
+ */
+async function generateEpub(title, chapters, output) {
+    const options = {
+        title: title,
+        content: chapters,
+        verbose: false,
+        output,
+    };
+    await new Epub(options);
+}
+
+/**
+ *
+ * @param {string} service
+ * @param {import("nodemailer/lib/smtp-connection").Credentials} credentials
+ * @param {string} recipients
+ * @param {import("nodemailer/lib/mailer").Attachment[]} attachments
+ */
+async function sendFile(service, credentials, recipients, attachments) {
+    const transporter = nodemailer.createTransport({
+        service: service,
+        auth: credentials,
+    });
+    /** @type {import("nodemailer/lib/smtp-transport").MailOptions} */
+    const mailOptions = {
+        from: credentials.user,
+        to: recipients,
+        attachments,
+        html: '<div dir="auto"></div>',
+    };
+    transporter.sendMail(mailOptions);
+}
+
+async function run() {
+    dotenv.config();
+    const dateTimeStamp = new Date()
+        .toISOString()
+        .replace(/[-:T]/g, '')
+        .replace(/\.\d\d\dZ/, '');
+    const articleTitle = `${dateTimeStamp} Blog Feed`;
+    const fileName = `${dateTimeStamp}_BlogFeed.epub`;
+    const outputPath = `./out/${fileName}`;
+    const scrapedArticles = await Promise.all(
+        blogconfig.blogs.map((b) => scrapeArticle(b))
+    );
+    /** @type {import('epub-gen').Chapter[]} */
+    const chapters = [];
+    scrapedArticles.forEach((article) => {
+        if (article) {
+            chapters.push(article);
+        }
+    });
+    await generateEpub(articleTitle, chapters, outputPath);
+    const emailCredentials = {
+        user: process.env.EMAIL_USER || '',
+        pass: process.env.EMAIL_PASSWORD || '',
+    };
+    const fileAttachement = {
+        filename: fileName,
+        path: outputPath,
+        contentType: 'application/epub',
+    };
+    await sendFile(
+        process.env.EMAIL_PROVIDER || '',
+        emailCredentials,
+        process.env.EMAIL_RECIPIENTS || '',
+        [fileAttachement]
+    );
+}
+
+run();
